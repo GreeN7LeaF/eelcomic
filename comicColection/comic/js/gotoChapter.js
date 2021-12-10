79bbc0951d0d8422fff376ae5671ad5b5023144b
thaomy <thaomy310702@gmail.com>
2021-12-10 09:30:52 +0700

    update preChapter ád nextChapter function

---
+++ comicColection/comic/js/gotoChapter.js
@@ -0,0 +1,31 @@
+function preChapter(){
+    var thisHref = window.location.pathname;
+    var chapters = document.getElementsByClassName('danh-sach-chuong')[0].options.length;
+    // console.log(chapters);
+
+    var match = thisHref.match(/chap_(\d+)/)
+    if (match) {
+        if(match[1] > 1){
+            var temp = parseInt(match[1]) - 1;
+            var tmp = thisHref.replace(match[1], next);
+            window.location.href = tmp;
+        }
+    }
+
+}
+
+function nextChapter(){
+    var thisHref = window.location.pathname;
+    var chapters = document.getElementsByClassName('danh-sach-chuong')[0].options.length;
+    // console.log(chapters);
+
+    var match = thisHref.match(/chap_(\d+)/)
+    if (match) {
+        if(match[1] <= chapters){
+            var next = parseInt(match[1]) + 1;
+            var tmp = thisHref.replace(match[1], next);
+            window.location.href = tmp;
+        }
+    }
+
+}
