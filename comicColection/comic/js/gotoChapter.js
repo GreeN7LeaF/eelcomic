cc71b9e3504d34f9f117154f57faf823d83f4854
thaomy <thaomy310702@gmail.com>
2021-12-13 22:29:40 +0700

    update dark mode

--- comicColection/comic/js/gotoChapter.js
+++ comicColection/comic/js/gotoChapter.js
@@ -7,7 +7,7 @@ function preChapter(){
     if (match) {
         if(match[1] > 1){
             var pre = parseInt(match[1]) - 1;
-            var tmp = thisHref.replace(match[1], pre);
+            var tmp = "../" + thisHref.replace(match[1], pre);
             window.location.href = tmp;
         }
     }
@@ -16,6 +16,7 @@ function preChapter(){
 
 function nextChapter(){
     var thisHref = window.location.pathname;
+    console.log(window.location);
     var chapters = document.getElementsByClassName('danh-sach-chuong')[0].options.length;
     // console.log(chapters);
 
@@ -24,7 +25,8 @@ function nextChapter(){
         if(match[1] < chapters){
             var next = parseInt(match[1]) + 1;
             var tmp = thisHref.replace(match[1], next);
-            window.location.href = tmp;
+            console.log(`${tmp}`);
+            // window.location.href = tmp;
         }
     }
 
---
+++ js/chapter.js
@@ -0,0 +1,6 @@
+// novel mode
+// var novelMode = document.getElementById("novel-mode");
+// // var darkMode = novelMode.getElementById('darkMode');
+// // var lightMode = novelMode.getElementById('lightMode');
+
+// console.log(document.getElementById("novel-mode"));
