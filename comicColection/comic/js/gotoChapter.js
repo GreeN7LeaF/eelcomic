8ac285fdd48bad7e7804879f0cdf78182b2d75d7
thaomy <thaomy310702@gmail.com>
2021-12-13 23:20:09 +0700

    update prechapter and nextchapter

--- comicColection/comic/js/gotoChapter.js
+++ comicColection/comic/js/gotoChapter.js
@@ -1,32 +1,41 @@
 function preChapter(){
-    var thisHref = window.location.pathname;
-    var chapters = document.getElementsByClassName('danh-sach-chuong')[0].options.length;
-    // console.log(chapters);
+    var thisHref = window.location.href;
+    var url = thisHref.substring(21);
+    var head = "../../../..";
+    // console.log(url);
+    // console.log(head);
 
-    var match = thisHref.match(/chap_(\d+)/)
+    var chapters = document.getElementsByClassName('danh-sach-chuong')[0].options.length;
+
+    var match = url.match(/chap_(\d+)/)
     if (match) {
-        if(match[1] > 1){
+        if(match[1] < chapters){
             var pre = parseInt(match[1]) - 1;
-            var tmp = "../" + thisHref.replace(match[1], pre);
-            window.location.href = tmp;
+            var tmp = url.replace(match[1], pre);
+
+            var go = head + tmp;
+            window.location.href = go;
         }
     }
-
 }
 
 function nextChapter(){
-    var thisHref = window.location.pathname;
-    console.log(window.location);
-    var chapters = document.getElementsByClassName('danh-sach-chuong')[0].options.length;
-    // console.log(chapters);
+    var thisHref = window.location.href;
+    var url = thisHref.substring(21);
+    var head = "../../../..";
+    console.log(url);
+    console.log(head);
 
-    var match = thisHref.match(/chap_(\d+)/)
+    var chapters = document.getElementsByClassName('danh-sach-chuong')[0].options.length;
+
+    var match = url.match(/chap_(\d+)/)
     if (match) {
         if(match[1] < chapters){
             var next = parseInt(match[1]) + 1;
-            var tmp = thisHref.replace(match[1], next);
-            console.log(`${tmp}`);
-            // window.location.href = tmp;
+            var tmp = url.replace(match[1], next);
+
+            var go = head + tmp;
+            window.location.href = go;
         }
     }
 
