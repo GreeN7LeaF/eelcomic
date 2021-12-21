148e5ae71ffc18d29cb230c894474fcf894ad173
thaomy <thaomy310702@gmail.com>
2021-12-21 16:26:25 +0700

    update comment

--- js/general.js
+++ js/general.js
@@ -83,35 +83,53 @@ function hello(){
 
 // comment
 function upComment(){
-    var input = document.querySelector('#content-comment').value;
-    var list = document.querySelector('#showComments');
-    var childs = list.children;
-    var name = localStorage['currUser'];
+    if(localStorage['currUser'] != 'user'){
+        var input = document.querySelector('#content-comment').value;
+        var list = document.querySelector('#showComments');
+        var childs = list.children;
+        var name = localStorage['currUser'];
 
-    var tmp = childs[childs.length-1];
-    var nameC = tmp.querySelector('.detail-list-comment-title').firstElementChild;
-    var content = tmp.querySelector('.detail-list-comment-content');
-    var time = tmp.querySelector('.detail-list-comment-title').lastElementChild;
-    // change infor of last comment
-    content.innerHTML = input;
-    nameC.innerHTML = name;
-    time.innerHTML = "1 giây trước";
-    document.querySelector('#content-comment').value = '';
-    // remove last child from list
-    list.removeChild(childs[childs.length-1]);
-    // add list to temp array to reverse
-    var t = []; 
-    for(var i = 0; i < childs.length; i++){
-        t.push(childs[i]);
-    }
+        var tmp = childs[childs.length-1];
+        var nameC = tmp.querySelector('.detail-list-comment-title').firstElementChild;
+        var content = tmp.querySelector('.detail-list-comment-content');
+        var time = tmp.querySelector('.detail-list-comment-title').lastElementChild;
+        // change infor of last comment
+        content.innerHTML = input;
+        nameC.innerHTML = name;
+        time.innerHTML = "1 giây trước";
+        document.querySelector('#content-comment').value = '';
+        // remove last child from list
+        list.removeChild(childs[childs.length-1]);
+        // add list to temp array to reverse
+        var t = []; 
+        for(var i = 0; i < childs.length; i++){
+            t.push(childs[i]);
+        }
 
-    // clear all DOM curr in list
-    while(list.firstElementChild){
-        list.removeChild(list.firstChild);
-    }
-    // append child form array to list DOM .. and done :333 (lam lan 2 trong bat luc)
-    list.appendChild(tmp);
-    for(var i = 0; i < t.length; i++){
-        list.appendChild(t[i]);
+        // clear all DOM curr in list
+        while(list.firstElementChild){
+            list.removeChild(list.firstChild);
+        }
+        // append child form array to list DOM .. and done :333 (lam lan 2 trong bat luc)
+        list.appendChild(tmp);
+        for(var i = 0; i < t.length; i++){
+            list.appendChild(t[i]);
+        }
+    }else{
+        window.location.href = 'login.html';
     }
 }
+function comment_reaction(val, ele){
+    var tmp = ele.parentNode;
+    var target = "comment_reaction_" + val;
+    var child = tmp.children;
+
+    for(var i = 0; i < child.length; i++){
+      if(child[i].id == target){
+        var get = parseInt(child[i].innerHTML, 10);
+        get++;
+        child[i].innerHTML = get;
+        break; 
+      }
+    }
+  }
